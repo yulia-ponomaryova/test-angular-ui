eb9692713c6d589df71c0b4a3a1f5c48f7bd1d65
Yulia Ponomaryova <yulia.ponomaryova@getgsi.com>
2022-09-19 12:05:12 +0400

    SUB-2: App Header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BookItemComponent } from './bookshelf/book-item/book-item.component';
 import { BookshelfComponent } from './bookshelf/bookshelf.component';
+import { HeaderComponent } from './header/header.component';
 
 import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookItemComponent,
-    BookshelfComponent
+    BookshelfComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
